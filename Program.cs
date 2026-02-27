5d659b898780ed1743fe61bd0a0ce970a96dabd3
naj012025 <naj012025@gmail.com>
2026-02-27 14:30:45 +0100

    Oppgave 2 backend

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Uke_2_lekser;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        
+        DateTime Now = DateTime.Now;
+        int time = Now.Hour;
+        Console.WriteLine("Hello user please enter your name: ");
+        string? username =  Console.ReadLine();// runs men får warning og spør ikke brukere om navn.
+        Console.Clear();
+        Console.WriteLine($"Hello {username}\n The Date and time is {Now}");
+            // husk at <= betyr mindre eller equal for exempel 10. 
+        switch(time)
+        {
+           case int T when  T <= 10:
+           Console.WriteLine("Good morning!");
+            break;
+            case int T when T <= 12:
+            Console.WriteLine("Lunchtime");
+            break;
+           case int T when T <= 14:
+           Console.WriteLine("Good afternoon");
+           break;
+           case int T when T <= 20:
+           Console.WriteLine("good evening");
+           break;
+           default:
+           Console.WriteLine("you should be sleeping.");
+           break;
+        
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,19 @@
+
+// planen for oppgaven.
+Oppgave 2 greeter med date time er planen
+Skal kunne gi current time og gi en greeting for example good morning /good afternon/go to sleep etc.
+trengs ikke for challange men user input for navn for exempel spør user input your name så spyr den ut good morning navn eller en av de andre maybe. via Console.Readline().
+Husk og begyn med stor D for date time. skrev med liten først fant ingenting.
+
+//
+Har fikset switch case sikkert overboard for uke 2 oppgave men lettere følte jeg en if og else if etc.
+
+//
+lagdt til en user input for og spørre om navn og etter navn clearer den console kom på at jørgen gjorde det en gang. 
+så gir den en hello navn og gir dato og tid til brukeren.
+
+//
+lærte om switch og bool i dag så måtte google meg frem til date and time men det sitter nå 
+men addet mine egne ting etc og skrev i blinde etter jeg leste.
+så etter jeg var ferdig keywordsene jeg trenger sto fakisk i oppgave texten så gjorde det vanskligere for meg selv men kom i mål til slut. Time used 55 mins ish.
+
